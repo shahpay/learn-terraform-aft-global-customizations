765a386c0ef2bd8748b92fce4a302df762976a8c
shahpay <shahpay@amazon.co.uk>
2023-06-06 16:11:43 +0100

    modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,8 @@ data "aws_region" "eu-west-1" {
     provider = aws.euwest1
 }
 
+data "aws_caller_identity" "current" {}
+
 resource "aws_securityhub_standards_subscription" "nist_benchmarks" {
   provider = aws.euwest2
   standards_arn = "arn:aws:securityhub:${data.aws_region.eu-west-2.name}::standards/nist-800-53/v/5.0.0"
@@ -14,4 +16,11 @@ resource "aws_securityhub_standards_subscription" "nist_benchmarks" {
 resource "aws_securityhub_standards_subscription" "nist_benchmarks-euwest1" {
   provider = aws.euwest1
   standards_arn = "arn:aws:securityhub:${data.aws_region.eu-west-1.name}::standards/nist-800-53/v/5.0.0"
+}
+
+resource "aws_securityhub_standards_control" "preventthis" {
+  provider = aws.euwest1
+  standards_control_arn = "arn:aws:securityhub:${data.aws_region.eu-west-1.name}:${data.aws_caller_identity.current}:control/cis-aws-foundations-benchmark/v/1.2.0/CloudTrail.6"
+  control_status        = "DISABLED"
+  disabled_reason       = "We handle password policies within Okta"
 }
