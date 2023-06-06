2baf4c9b0daf8d0dce13a00576b5a9029dfb7fdc
shahpay <shahpay@amazon.co.uk>
2023-06-06 12:14:08 +0100

    modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,6 @@
+data "aws_region" "current" {}
+
 resource "aws_securityhub_standards_subscription" "nist_benchmarks" {
   provider = aws.euwest2
-  standards_arn = "arn:aws:securityhub:::standards/nist-800-53/v/5.0.0"
+  standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/nist-800-53/v/5.0.0"
 }
