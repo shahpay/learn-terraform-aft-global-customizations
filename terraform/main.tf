36d0c8aba41e5a1a9986f487c804597a4685846c
shahpay <shahpay@amazon.co.uk>
2023-06-06 12:04:05 +0100

    modified the aft global-customization

---
+++ terraform/main.tf
@@ -0,0 +1,4 @@
+resource "aws_securityhub_standards_subscription" "nist_benchmarks" {
+  provider = aws.euwest2
+  standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/nist-800-53/v/5.0.0"
+}
