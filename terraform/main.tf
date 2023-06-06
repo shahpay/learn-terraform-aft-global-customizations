793bd5996376c669cc70ca8981d3a244b67183cc
shahpay <shahpay@amazon.co.uk>
2023-06-06 12:38:11 +0100

    modified main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,16 @@ data "aws_region" "eu-west-2" {
     provider = aws.euwest2
 }
 
+data "aws_region" "eu-west-1" {
+    provider = aws.euwest1
+}
+
 resource "aws_securityhub_standards_subscription" "nist_benchmarks" {
   provider = aws.euwest2
   standards_arn = "arn:aws:securityhub:${data.aws_region.eu-west-2.name}::standards/nist-800-53/v/5.0.0"
+}
+
+resource "aws_securityhub_standards_subscription" "nist_benchmarks" {
+  provider = aws.euwest1
+  standards_arn = "arn:aws:securityhub:${data.aws_region.eu-west-1.name}::standards/nist-800-53/v/5.0.0"
 }
