790514325c06e89429ef17d756b2ba9413a6bd36
shahpay <shahpay@amazon.co.uk>
2023-06-08 08:54:28 +0100

    modified enabled nist

--- terraform/enable_nist.tf
+++ terraform/enable_nist.tf
@@ -82,10 +82,13 @@ data "aws_region" "eu-west-2" {
     provider = aws.euwest2
 }
 
-# resource "aws_securityhub_standards_subscription" "nist_benchmarks-us-east-1"  {
-#   provider = aws.sts-root
-#   standards_arn = "arn:aws:securityhub:${data.aws_region.us-east-1.name}::standards/nist-800-53/v/5.0.0"
-# }
+data "aws_region" "us-east-1" {
+    provider = aws.euwest2
+}
+
+resource "aws_securityhub_standards_subscription" "nist_benchmarks-us-east-1"  {
+  standards_arn = "arn:aws:securityhub:${data.aws_region.us-east-1.name}::standards/nist-800-53/v/5.0.0"
+}
 
 resource "aws_securityhub_standards_subscription" "nist_benchmarks-us-east-2" {
   provider = aws.useast2
