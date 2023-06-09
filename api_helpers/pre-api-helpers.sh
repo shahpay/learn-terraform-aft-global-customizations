80cfe4b451eb499db4e4a57f5a39cb47b972e54d
shahpay <shahpay@amazon.co.uk>
2023-06-09 13:31:14 +0100

    modified prehelper

--- api_helpers/pre-api-helpers.sh
+++ api_helpers/pre-api-helpers.sh
@@ -6,9 +6,9 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-production_ou=Production
+# production_ou=Production
 get_accounts_recursive() {
-  accounts="$(aws organizations list-accounts-for-parent --parent-id "$production_ou" --output text --query 'Accounts[][Id]')"
+  accounts="$(aws organizations list-accounts-for-parent --parent-id "Production" --output text --query 'Accounts[][Id]')"
   for ou in $(aws organizations list-organizational-units-for-parent --parent-id "$production_ou" --output text --query 'OrganizationalUnits[][Id]'); do
     accounts="$accounts $(get_accounts_recursive "$ou")"
   done
