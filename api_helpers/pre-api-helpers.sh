b83f611141ea646dae90e81fa9046a3ec80cf004
shahpay <shahpay@amazon.co.uk>
2023-06-09 12:54:32 +0100

    modified main.tf

--- api_helpers/pre-api-helpers.sh
+++ api_helpers/pre-api-helpers.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
 echo "Executing Pre-API Helpers"
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+production_ou = Production
+get_accounts_recursive() {
+  accounts="$(aws organizations list-accounts-for-parent --parent-id "$production_ou" --output text --query 'Accounts[][Id]')"
+  for ou in $(aws organizations list-organizational-units-for-parent --parent-id "$production_ou" --output text --query 'OrganizationalUnits[][Id]'); do
+    accounts="$accounts $(get_accounts_recursive "$ou")"
+  done
+  echo "$accounts" | xargs
+}
+
+get_accounts_recursive
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,11 @@
-# data "aws_region" "eu-west-2" {
-#     provider = aws.euwest2
-# }
-
-# data "aws_region" "eu-west-1" {
-#     provider = aws.euwest1
-# }
-
-# data "aws_caller_identity" "current" {}
-
-# resource "aws_securityhub_standards_subscription" "nist_benchmarks" {
-#   provider = aws.euwest2
-#   standards_arn = "arn:aws:securityhub:${data.aws_region.eu-west-2.name}::standards/nist-800-53/v/5.0.0"
-# }
-
-# resource "aws_securityhub_standards_subscription" "nist_benchmarks-euwest1" {
-#   provider = aws.euwest1
-#   standards_arn = "arn:aws:securityhub:${data.aws_region.eu-west-1.name}::standards/nist-800-53/v/5.0.0"
-# }
-
-# resource "aws_securityhub_standards_subscription" {
-#   provider = aws.euwest1
-#   standards_arn = "arn:aws:securityhub:${data.aws_region.eu-west-1.name}::standards/nist-800-53/v/5.0.0"
-# }
-
-
+module securityhub {
+  providers = {
+    aws.useast2 = aws.useast2
+    aws.uswest2 = aws.uswest2
+    aws.cacentral1 = aws.cacentral1
+    aws.eucentral1 = aws.eucentral1
+    aws.euwest1 = aws.euwest1
+    aws.euwest2 = aws.euwest2
+}
+  source = "/Users/shahpay/Desktop/Flutter/learn-aft/payal-aft/learn-terraform-aft-account-customizations/dtpl-dev/terraform/modules/securityhub/modules/securityhub"
+}
