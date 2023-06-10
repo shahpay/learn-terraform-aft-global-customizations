63ab9646dc0105796bcae9170c0cf1997c203674
shahpay <shahpay@amazon.co.uk>
2023-06-10 16:17:43 +0100

    pushed the value

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
-module securityhub {
-  providers = {
-    aws.useast2 = aws.useast2
-    aws.uswest2 = aws.uswest2
-    aws.cacentral1 = aws.cacentral1
-    aws.eucentral1 = aws.eucentral1
-    aws.euwest1 = aws.euwest1
-    aws.euwest2 = aws.euwest2
-}
-  source = "git@github.com:shahpay/learn-terraform-aft-account-customizations.git/dtpl-dev/terraform/modules/securityhub"
-}
+# module securityhub {
+#   providers = {
+#     aws.useast2 = aws.useast2
+#     aws.uswest2 = aws.uswest2
+#     aws.cacentral1 = aws.cacentral1
+#     aws.eucentral1 = aws.eucentral1
+#     aws.euwest1 = aws.euwest1
+#     aws.euwest2 = aws.euwest2
+# }
+#   source = "git@github.com:shahpay/learn-terraform-aft-account-customizations.git/dtpl-dev/terraform/modules/securityhub"
+# }
