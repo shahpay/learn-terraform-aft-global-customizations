467e7cb99811825c7bbb2b4911ac503f7b14837c
shahpay <shahpay@amazon.co.uk>
2023-06-12 05:47:03 +0100

    modified securityhub modules

--- terraform/modules/securityhub/main.tf
+++ terraform/modules/securityhub/main.tf
@@ -14,34 +14,6 @@ locals  {
  disabled_reason = "Not applicable for the environment"
 }
 
-data "aws_region" "us-east-1" {}
-
-data "aws_region" "us-east-2" {
-  provider = aws.useast2
-}
-
-data "aws_region" "us-west-2" {
-  provider = aws.uswest2
-}
-
-data "aws_region" "ca-central-1" {
-  provider = aws.cacentral1
-}
-
-data "aws_region" "eu-central-1" {
-  provider = aws.eucentral1
-}
-
-data "aws_region" "eu-west-1" {
-  provider = aws.euwest1
-}
-
-data "aws_region" "eu-west-2" {
-  provider = aws.euwest2
-}
-
-
-data "aws_caller_identity" "current_account" {}
 
 resource "aws_securityhub_standards_control" "evaluate_controls_useast1" {
    for_each = var.disabled_nis_control_all_region
