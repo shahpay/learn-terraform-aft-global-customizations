6f25787bdcfab631d7832ff229fa911cf43a4b56
shahpay <shahpay@amazon.co.uk>
2023-06-14 08:05:00 +0100

    modified the values

--- terraform/disable_specific_nist_controls.tf
+++ terraform/disable_specific_nist_controls.tf
@@ -62,3 +62,25 @@ module securityhub_excluding_production_accounts{
   aws_securityhub_standards_subscription.nist_benchmarks-eu-central-1
 ]
 }
+
+module securityhub_excluding_global_region{
+  providers = {
+    aws.useast2 = aws.useast2
+    aws.uswest2 = aws.uswest2
+    aws.cacentral1 = aws.cacentral1
+    aws.eucentral1 = aws.eucentral1
+    aws.euwest1 = null
+    aws.euwest2 = aws.euwest2
+}
+  # count = var.environment != "prod" ? 1 : 0
+  source = "./modules/securityhub"
+  disabled_nis_control_all_region = {}
+  depends_on = [ aws_securityhub_standards_subscription.nist_benchmarks-eu-west-1,
+  aws_securityhub_standards_subscription.nist_benchmarks-eu-west-2,
+  aws_securityhub_standards_subscription.nist_benchmarks-us-east-1,
+  aws_securityhub_standards_subscription.nist_benchmarks-us-west-2,
+  aws_securityhub_standards_subscription.nist_benchmarks-us-east-2,
+  aws_securityhub_standards_subscription.nist_benchmarks-ca-central-1,
+  aws_securityhub_standards_subscription.nist_benchmarks-eu-central-1
+]
+}
