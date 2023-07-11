01744434cedccd9f79e05eb40445a4e00871702e
shahpay <shahpay@amazon.co.uk>
2023-07-11 20:49:34 +0100

    modiify the values

--- terraform/disable_specific_nist_controls.tf
+++ terraform/disable_specific_nist_controls.tf
@@ -144,7 +144,7 @@ data "aws_caller_identity" "current_account" {}
 resource "aws_securityhub_standards_control" "ev" {
    control_status = "DISABLED"
    disabled_reason = "modified the values"
-   standards_control_arn = "arn:aws:securityhub:eu-west-1:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/5.0.0/EC2.2"
+   standards_control_arn = "arn:aws:securityhub:eu-west-1:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/5.0.0/EC2.29"
 }
 
 # resource "aws_securityhub_standards_control" "ec2_2" {
