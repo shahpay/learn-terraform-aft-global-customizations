e9025a550c09f93f2149ca3158a9858dd845d863
shahpay <shahpay@amazon.co.uk>
2023-07-11 13:40:12 +0100

    modified the values

--- terraform/modules/securityhub/main.tf
+++ terraform/modules/securityhub/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.20.0"
+      version = "5.4.0"
       configuration_aliases = [aws.useast2, aws.uswest2, aws.cacentral1, aws.eucentral1, aws.euwest1, aws.euwest2, aws.euwest3 ]
     }
   }
