f847c816d0b21a05c9c66517a680afd8e15cfab4
shahpay <shahpay@amazon.co.uk>
2023-07-11 13:00:20 +0100

    modifed the values

--- terraform/disable_specific_nist_controls.tf
+++ terraform/disable_specific_nist_controls.tf
@@ -144,7 +144,7 @@ data "aws_caller_identity" "current_account" {}
 resource "aws_securityhub_standards_control" "ev" {
    control_status = "DISABLED"
    disabled_reason = local.disabled_reason
-   standards_control_arn = "arn:aws:securityhub:eu-west-1:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/${local.nis_version}/EC2.2"
+   standards_control_arn = "arn:aws:securityhub:eu-west-1:${data.aws_caller_identity.current_account.account_id}:control/nist-800-53/v/5.0.0/EC2.2"
 }
 
 # module securityhub_core_accounts_excluding_global_region{
