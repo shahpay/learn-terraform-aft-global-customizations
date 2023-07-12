fc1bc01faeea1b7282d11843c2326e417a6aa0bc
shahpay <shahpay@amazon.co.uk>
2023-07-12 01:54:46 +0100

    modifiy

--- api_helpers/post-api-helpers.sh
+++ api_helpers/post-api-helpers.sh
@@ -4,4 +4,5 @@ echo "Executing Post-API Helpers"
 echo $DEFAULT_PATH
 ls
 ls $DEFAULT_PATH
+echo $TF_VAR_environment
 python3 $DEFAULT_PATH/api_helpers/python/main.py "Prod" "Noncore" $VENDED_ACCOUNT_ID
